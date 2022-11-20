e8c59440ecf6f46f6d867d8e4f6d920d1e437166
AlonsoPrez01 <alonso.perflores@gmail.com>
2022-11-20 14:23:56 -0500

    Auth

--- lib/compoentes/screens.dart
+++ lib/compoentes/screens.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:anahuac_news/compoentes/zedess_api.dart';
 import 'package:anahuac_news/compoentes/estilos/style_text.dart';
